ac4fb04df75aa9e9f33391fd43cd98c942e075eb
cainiaocome <cainiaocome@gmail.com>
2016-01-04 11:40:18 +0800

    sql

--- utils.py
+++ utils.py
@@ -2,7 +2,15 @@
 
 import string
 import random
+import pymysql
 
 def random_string_gen(size=8):
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(size))
+
+def execute_sql(db='mail', sql=''):
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='jialin,0204', db=db)
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    cur.execute(sql)
+    res = cur.fetchall()
+    return res
